eadf2e9d004fa03da46768b395e340a1cf5993cb
harsh-mmc <Harsh.Poonia@marsh.com>
2022-12-27 20:15:57 +0000

    Variables and Table setup upon startup

---
+++ app/variables_setup.py
@@ -0,0 +1,11 @@
+import boto3
+
+BUCKET_NAME = 'labs-smart-contract-security-audit'
+CONTRACT_FOLDER = 'core-contracts'
+TEMP_FILE_NAME = 'temporary'
+TABLE_NAME = 'Smart-Contract-Audit'
+USER_TABLE = 'Smart-Contract-Users'
+SLITHER_URL = 'http://127.0.0.1:8001'
+
+s3 = boto3.resource('s3')
+db = boto3.resource('dynamodb', region_name = 'us-east-2')
